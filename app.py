4aa673c96fa108f2714154e48ec13e8f4180cc2f
Atharva Mishra <atharvamishra956@gmail.com>
2021-05-04 11:24:03 +0530

    added title

--- app.py
+++ app.py
@@ -11,3 +11,6 @@ style.layout('TNotebook.Tab', [])
 
 main_frame = Frame(root, bg="#181818")
 main_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
+
+title_label = Label(main_frame, bg="#ff0000", text="YT Downloader", font=("Courier", 28))
+title_label.place(relx=0, rely=0, relwidth=1, relheight=0.1)
