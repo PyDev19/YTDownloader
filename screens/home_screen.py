6381596518d54cad74dcdac2e9195e7210a78529
Atharva Mishra <atharvamishra956@gmail.com>
2021-05-06 18:47:37 +0530

    changed styles or title and title frame

--- screens/home_screen.py
+++ screens/home_screen.py
@@ -3,9 +3,9 @@ from tkinter import *
 
 def load(root, app_icon):
     title_frame = Frame(root, bg="#21252B")
-    title_frame.place(relx=0, rely=0, relwidth=1, height=75)
+    title_frame.place(relx=0, rely=0, relwidth=1, height=45)
 
-    title_label = Label(root, text="YT Downloader", bg="#21252B", fg="#fff", font=("Courier", 50))
+    title_label = Label(root, text="YT Downloader", bg="#21252B", fg="#fff", font=("Courier", 30))
     title_label.place(relx=0, rely=0, relwidth=1)
 
     icon_label = Label(root, bg="#282C34", image=app_icon)
