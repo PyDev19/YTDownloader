a5e32feca6e5c5b30cfff6808263a284bc66c20a
PyDev19 <atharvamishra956+1@gmail.com>
2021-10-25 10:28:08 +0530

    added check internet function to backend

--- backend.py
+++ backend.py
@@ -1,11 +1,22 @@
 import os
 
 import requests
-from PySide6.QtCore import Slot
+from PySide6.QtCore import Slot, QObject
 from pytube import YouTube, exceptions
 
-
-class BackEnd:
+class BackEnd(QObject):
     def __init__(self, app):
+        super().__init__()
         self.app = app
         self.file_size = 0
+    
+    @Slot(result=bool)
+    def check_internet(self): 
+        url = "https://yamiatem.github.io/YTDownloader/"
+        timeout = 5
+
+        try:
+            request = requests.get(url, timeout=timeout)
+            return True
+        except (requests.ConnectionError, requests.Timeout) as exception:
+            return False
