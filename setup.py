59ac005d0d5fca65f43ec09d422b65015960b926
PyDev19 <atharvamishra956+1@gmail.com>
2022-12-25 21:57:22 +0530

    updated to remove unnecessary packages

--- setup.py
+++ setup.py
@@ -3,9 +3,17 @@ import sys
 from cx_Freeze import setup, Executable
 
 # ADD FILES
-files = ['icons/', 'menu_bar.py', 'main.qss', 'menu_bar.qss', 'screens/', 'menu_icons/']
+files = ['qml_components/', 'backend.py', 'resources_rc.py', 'menu_icons/', 'icons/', 'main.qml']
+packages = ["sys", "PySide6.QtGui", "PySide6.QtQml", "PySide6.QtCore", "pytube", "requests"]
+exclude = ["tkinter", "asyncio", "concurrent", "ctypes", "distutils", 
+           "email", "html", "http", "lib2to3", "multiprocessing", 
+           "pydoc_data", "test", "unittest", "xml", "xmlrpc"]
 
-build_exe_options = {"packages": ["os"], 'include_files': files}
+build_exe_options = {
+    "packages": packages,
+    "include_files": files,
+    "excludes": exclude
+}
 
 base = None
 if sys.platform == "win32":
